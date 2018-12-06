ddb717016626aaa8e3fe02d2680b95653faa7955
d-derks <2debbiederks@gmail.com>
2018-12-06 20:54:54 +0100

    Updates

---
+++ precache-manifest.eef8fd91e5f612415595130cb9706cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04a88bf64b01662a04852bb4ee79410e",
+    "url": "/pincamp-project/static/media/camping-area.04a88bf6.jpg"
+  },
+  {
+    "revision": "22d0d102e7de520bf16d",
+    "url": "/pincamp-project/static/js/runtime~main.22d0d102.js"
+  },
+  {
+    "revision": "8c6d1ad757d235a8e3a6",
+    "url": "/pincamp-project/static/js/main.8c6d1ad7.chunk.js"
+  },
+  {
+    "revision": "d94fc685c375ad0a6b02",
+    "url": "/pincamp-project/static/js/1.d94fc685.chunk.js"
+  },
+  {
+    "revision": "8c6d1ad757d235a8e3a6",
+    "url": "/pincamp-project/static/css/main.25b462bb.chunk.css"
+  },
+  {
+    "revision": "0124e350a1d0fea6853ed8beddecd8be",
+    "url": "/pincamp-project/index.html"
+  }
+];
